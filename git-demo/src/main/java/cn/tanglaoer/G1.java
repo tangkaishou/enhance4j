616556c009baefeb5e3b2628d896af6f1a3ce33f
tanglaoer <1585701160@qq.com>
2023-08-29 21:16:04 +0800

    添加git测试项目

---
+++ git-demo/src/main/java/cn/tanglaoer/G1.java
@@ -0,0 +1,11 @@
+package cn.tanglaoer;
+
+/**
+ * @author <a href="https://github.com/TangLaoEr">tks</a>
+ * @Date 2023/8/29
+ */
+public class G1 {
+    public static void main(String[] args) {
+        System.out.println("main");
+    }
+}
